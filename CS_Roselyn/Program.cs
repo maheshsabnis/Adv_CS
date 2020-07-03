0e812453d91a04c6548cb2c75baaf2e86720fe49
maheshsabnis <sabnis_m@hotmail.com>
2020-07-03 14:16:04 +0530

    Roselyn

---
+++ CS_Roselyn/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using RoslynCore;
+using System;
+
+namespace CS_Roselyn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenerateSampleViewModel();
+        }
+        static void GenerateSampleViewModel()
+        {
+            const string models = @"namespace Models
+{
+  public class Item
+  {
+    public string ItemName { get; set }
+  }
+}
+";
+            var node = CSharpSyntaxTree.ParseText(models).GetRoot();
+            Console.WriteLine(node.ToFullString());
+           
+            //var viewModel = ViewModelGeneration.GenerateViewModel(node);
+            //if (viewModel != null)
+            //    Console.WriteLine(viewModel.ToFullString());
+            Console.ReadLine();
+        }
+    }
+
+
+  
+}
